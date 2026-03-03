6af62b3b2afd3cf5e086eba503431302364da7b0
Vaani Sharma <vaanis1110@gmail.com>
2026-03-03 16:06:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://chat-app-lemon-zeta.vercel.app",
+      "https://chat-application-peach-xi.vercel.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,
@@ -67,7 +67,7 @@ const io = new Server(httpServer, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://chat-app-lemon-zeta.vercel.app",
+      "https://chat-application-peach-xi.vercel.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,
